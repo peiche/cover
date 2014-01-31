eba2b1a50544c6216d8df5221319d7e5fc86fe11
Paul <peiche@gmail.com>
2014-01-31 10:56:27 -0600

    Reordered post meta.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -82,7 +82,7 @@ function beats_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	printf( __( '<span class="posted-on">Posted on <i class="fa fa-clock-o"></i> %1$s</span><span class="byline"> by %2$s</span>', 'beats' ),
+	printf( __( '<span class="posted-on">Posted by %2$s on <i class="fa fa-clock-o"></i> %1$s</span><span class="byline"></span>', 'beats' ),
 		sprintf( '<a href="%1$s" rel="bookmark">%2$s</a>',
 			esc_url( get_permalink() ),
 			$time_string
