de930a6756704ed76cfc4252b2234d7c6072c148
Paul <peiche@gmail.com>
2014-01-31 13:10:06 -0600

    Header nav stuff. More to come!

--- header.php
+++ header.php
@@ -25,12 +25,16 @@
 
 <body <?php body_class(); ?>>
 
+<header class="title">
+	<a href="/"><i class="fa fa-bookmark-o fa-lg"></i><span>eichefam.net</span></a>
+</header>
+
 <?php if (is_single() && '' != get_the_post_thumbnail()) { ?>
 	<div id="cover" class="cover featured-image">
 		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 		<div class="background" style="background-image: url('<?php echo $image[0]; ?>');"></div>
 		<header>
-			<h2><?php the_category(', ', 'multiple') ?></h2>
+			<h2><?php the_category(' / ', 'multiple') ?></h2>
 			<h1><a href="#"><?php the_title(); ?></a></h1>
 			<span>
 				<?php beats_posted_on(); ?>
@@ -39,10 +43,9 @@
 	</div>
 <?php } ?>
 
-
-
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
+	<!--
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -54,7 +57,9 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'beats' ); ?></a>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		</nav>
+	</header>
+	-->
+	<!-- #masthead -->
 	
 	<div id="content" class="site-content">
