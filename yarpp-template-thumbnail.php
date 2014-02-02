52e66eb6ec584dc46a7a593adca2c9b1b238dbac
Paul <peiche@gmail.com>
2014-02-02 04:14:10 -0600

    Enabled related post even if no featured image is available.

--- yarpp-template-thumbnail.php
+++ yarpp-template-thumbnail.php
@@ -6,20 +6,19 @@ Author: mitcho (Michael Yoshitaka Erlewine)
 */ ?>
 <?php if (have_posts()) { ?>
 	<?php while (have_posts()) : the_post(); ?>
-		<?php if ( '' != get_the_post_thumbnail()) { ?>
-			<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-				<?php
-					if ( '' != get_the_post_thumbnail()) {
-						$image_arr = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-						$image = $image_arr[0];
-					}
-				?>
-				<span class="related-cover" style="background-image: url('<?php echo $image; ?>');"></span>
-				<span class="related-title">
-					<h2 class="site-title">Further reading</h2>
-					<h1 class="site-title"><?php the_title(); ?></h1>
-				</span>
-			</a>
-		<?php } ?>
+		<div class="related-post<?php if ( '' != get_the_post_thumbnail()) { ?> featured-image<?php } ?>">
+			<a class="cover-link" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"></a>
+			<?php
+				if ( '' != get_the_post_thumbnail()) {
+					$image_arr = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+					$image = $image_arr[0];
+				}
+			?>
+			<div class="related-cover" <?php if ( '' != get_the_post_thumbnail()) { ?>style="background-image: url('<?php echo $image; ?>');"<?php } ?>></div>
+			<div class="related-title">
+				<h2>Related</h2>
+				<h1><a href="#"><?php the_title(); ?></a></h1>
+			</div>
+		</div>
 	<?php endwhile; ?>
 <?php } ?>
