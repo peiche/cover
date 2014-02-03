ecf43b13e365b4f4db183bd8e0c3b5e427cbe998
Paul <peiche@gmail.com>
2014-02-03 15:59:33 -0600

    Theme-specific search form.

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<label>
+		<span class="screen-reader-text">Search for:</span>
+		<input type="search" class="search-field" placeholder="Search..." value="" name="s" title="Search for:" />
+	</label>
+	<button class="btn btn-link" type="submit"><i class="fa fa-search"></i></button>
+</form>
