08613ad6c5263abccbaa7e79f85ba0894312075d
Paul <peiche@gmail.com>
2014-02-03 02:13:25 -0600

    Uncommented content footer.

--- content.php
+++ content.php
@@ -41,27 +41,16 @@
 		<?php } ?>
 	</div><!-- .entry-summary -->
 	
-	<!--
 	<footer class="entry-meta">
 		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'beats' ) );
-				if ( $categories_list && beats_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'beats' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
 			<?php
 				/* translators: used between list items, there is a space after the comma */
 				$tags_list = get_the_tag_list( '', __( ', ', 'beats' ) );
 				if ( $tags_list ) :
 			?>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'beats' ), $tags_list ); ?>
-			</span>
+			<div class="tags-links">
+				<?php printf( __( '<i class="fa fa-tag"></i> %1$s', 'beats' ), $tags_list ); ?>
+			</div>
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
 
@@ -71,7 +60,6 @@
 
 		<?php edit_post_link( __( 'Edit', 'beats' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer>
-	-->
 	<!-- .entry-meta -->
 	
 </article><!-- #post-## -->
