3aa88d4c8195e1e1f32e3cfcf13b7d96ed5d8aa4
Paul <peiche@gmail.com>
2014-02-08 04:04:47 -0600

    Removed id

--- parts/cover-post.php
+++ parts/cover-post.php
@@ -5,7 +5,7 @@
 ?>
 
 <?php if ('' != get_the_post_thumbnail()) { ?>
-	<div id="cover" class="cover featured-image">
+	<div class="cover featured-image">
 		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 		<div class="background" style="background-image: url('<?php echo $image[0]; ?>');"></div>
 		<header>
