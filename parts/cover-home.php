97d1c54d8fb31c16dd5a36626e8ff92d74a6176b
Paul <peiche@gmail.com>
2014-02-08 04:04:22 -0600

    Removed id, fixed validation errors

--- parts/cover-home.php
+++ parts/cover-home.php
@@ -8,7 +8,7 @@
 <?php while (have_posts()) : the_post(); ?>
 	<?php if ($count == 0 && '' != get_the_post_thumbnail()) { ?>
 
-		<div id="cover" class="cover featured-image auto home">
+		<div class="cover featured-image auto home">
 			<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 			<div class="background" style="background-image: url('<?php echo $image[0]; ?>');"></div>
 			<header>
@@ -25,7 +25,7 @@
 				</span>
 				<!--
 				<div class="entry-summary">
-					<?php the_content( __( 'Continue reading <span class=\"meta-nav\">&rarr;</span>', 'cover' ) ); ?>
+					<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'cover' ) ); ?>
 				</div>
 				-->
 			</header>
