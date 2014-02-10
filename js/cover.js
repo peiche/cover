757bf732dbc70535e8704fe3341110a9b220b7e4
Paul <peiche@gmail.com>
2014-02-10 11:47:34 -0600

    Removed unnecessary properties

--- js/cover.js
+++ js/cover.js
@@ -1,9 +1,7 @@
 jQuery(document).ready(function() {
 	
 	var homeSwiper = jQuery('#cover-home').swiper({
-		mode: 'horizontal',
 		loop: true,
-		//autoPlay: 1000,
 		noSwiping: true,
 		simulateTouch: false,
 		calculateHeight: true
