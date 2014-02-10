1971a21655d9eecb22a289c58a5b029bd005a433
Paul <peiche@gmail.com>
2014-02-10 09:10:56 -0600

    Added format icons

--- parts/postformat.php
+++ parts/postformat.php
@@ -11,4 +11,8 @@
 	<span>Video</span> <i class="fa fa-youtube-play"></i>
 <?php } else if ('quote' == $format){ ?>
 	<span>Quote</span> <i class="fa fa-quote-right"></i>
+<?php } else if ('image' == $format) { ?>
+	<span>Image</span> <i class="fa fa-picture-o"></i>
+<?php } else if ('link' == $format) { ?>
+	<span>Link</span> <i class="fa fa-link"></i>
 <?php } ?>
