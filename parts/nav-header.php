8ef69377ca245ba9aafb12347ee591ad2954397e
Paul <peiche@gmail.com>
2014-02-11 16:04:26 -0600

    Search form fix

--- parts/nav-header.php
+++ parts/nav-header.php
@@ -14,15 +14,25 @@
 		endwhile;
 	}
 ?>
-<nav id="site-navigation" class="main-navigation<?php if (((is_single() || is_page()) && '' != get_the_post_thumbnail()) || (is_home() && $has_sticky) || is_author() || is_category()) { ?> featured-image<?php } ?>" role="navigation">
+<nav id="site-navigation" class="main-navigation<?php if (((is_single() || is_page()) && '' != get_the_post_thumbnail()) || (is_home() && !is_paged() && $has_sticky) || is_author() || is_category()) { ?> featured-image<?php } ?>" role="navigation">
 	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 	<div>
 		<ul>
 			<li>
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<input type="search" class="search-field" placeholder="Search..." value="" name="s" title="Search for:" />
-	<button class="btn btn-link" type="submit"><i class="fa fa-search"></i></button>
-</form>
+				<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>" onsubmit="return _s(this);">
+					<input type="search" class="search-field" placeholder="Search..." value="" name="s" title="Search for:" />
+					<button class="btn btn-link" type="submit" name="submit"><i class="fa fa-search"></i></button>
+					
+					<script>
+					function _s(form) {
+						if ('' == form.s.value) {
+							form.s.focus();
+							return false;
+						}
+						return true;
+					}
+					</script>
+				</form>
 			</li>
 		</ul>
 	</div>
