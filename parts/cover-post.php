3a15dbd1709b95efc7305704a0066d06cd930531
Paul <peiche@gmail.com>
2014-02-12 15:04:01 -0600

    Added data attributes for skrollr

--- parts/cover-post.php
+++ parts/cover-post.php
@@ -7,7 +7,7 @@
 <?php if ('' != get_the_post_thumbnail()) { ?>
 	<div class="cover featured-image">
 		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-		<div class="background" style="background-image: url('<?php echo $image[0]; ?>');"></div>
+		<div class="background" style="background-image: url('<?php echo $image[0]; ?>');" data-top="background-position: 50% 50%;" data-top-bottom="background-position: 50% 100%;"></div>
 		<header>
 			<h2>
 				<?php the_category(', ') ?>
