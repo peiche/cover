48b29bf153faa460687406d37f4b3d7fc53bc136
Paul <peiche@gmail.com>
2014-02-12 11:37:24 -0600

    Removed aside from theme support

--- functions.php
+++ functions.php
@@ -46,7 +46,7 @@ function cover_setup() {
 	) );
 
 	// Enable support for Post Formats.
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+	add_theme_support( 'post-formats', array( 'image', 'video', 'quote', 'link' ) );
 
 	// Setup the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'cover_custom_background_args', array(
