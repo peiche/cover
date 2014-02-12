17471f02e85a0e5bdac4453550043eb918f599f8
Paul <peiche@gmail.com>
2014-02-12 03:58:41 -0600

    Reverted content part files
    back to main directory. Jetpack's Infinite Scroll depends on these
    files' default location.

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
  * @package Cover
  */
 
@@ -16,19 +8,30 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<?php 
+			/*
+			$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+			query_posts(array(
+				'post__not_in' => get_option('sticky_posts'),
+				'paged' => $paged
+			));
+			*/
+		?>
 		<?php if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
+				
 				<?php if ( !is_sticky() ) { ?>
 					<?php
-						get_template_part( 'parts/content', get_post_format() );
+						get_template_part( 'content', get_post_format() );
 					?>
 				<?php } ?>
+				
 			<?php endwhile; ?>
 			<?php cover_paging_nav(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'parts/content', 'none' ); ?>
+			<?php get_template_part( 'content', 'none' ); ?>
 
 		<?php endif; ?>
 
--- single.php
+++ single.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'parts/content', 'single' ); ?>
+			<?php get_template_part( 'content', 'single' ); ?>
 
 			<?php cover_post_nav(); ?>
 
