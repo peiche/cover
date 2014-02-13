1ee6d58659e1327db923b2b34418a9cb824dcdec
Paul <peiche@gmail.com>
2014-02-13 08:56:56 -0600

    Detect mobile before enabling skrollr

--- js/cover.js
+++ js/cover.js
@@ -43,8 +43,10 @@ jQuery(document).ready(function() {
 	}
 	
 	// cover parallax
-	skrollr.init({
-		forceHeight: false
-	});
+	if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+		skrollr.init({
+			forceHeight: false
+		});
+	}
 	
 });
