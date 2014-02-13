fce6557d67008416d1aa3122b27e5f9e47331473
Paul <peiche@gmail.com>
2014-02-13 17:21:57 -0600

    Removed footer menu

--- footer.php
+++ footer.php
@@ -15,9 +15,6 @@
 	<?php } ?>
 	
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="smallnav">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</div>
 		<div class="site-info">
 			<?php do_action( 'cover_credits' ); ?>
 			<?php printf( __( 'Powered by %s and %s', 'cover' ), '<a href="http://www.wordpress.org/" title="WordPress">WordPress</a>', '<a href="http://eichefam.net/projects/cover/" title="Cover theme by Paul Eiche">Cover</a>' ); ?>
