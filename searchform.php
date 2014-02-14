a21eb088fc5796569a7f93dfec7d30b40f966840
Paul <peiche@gmail.com>
2014-02-13 23:41:41 -0600

    Changed search form button.

--- searchform.php
+++ searchform.php
@@ -3,5 +3,5 @@
 		<span class="screen-reader-text">Search for:</span>
 		<input type="search" class="search-field" placeholder="Search..." value="" name="s" title="Search for:" />
 	</label>
-	<button class="btn btn-link" type="submit"><i class="fa fa-search"></i></button>
+	<button class="btn btn-default" type="submit">Go</button>
 </form>
