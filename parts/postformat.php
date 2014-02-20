8e7498eabb3043d7c3bdc62930a529362436f210
Paul <peiche@gmail.com>
2014-02-20 12:58:08 -0600

    Removed sticky post icon

--- parts/postformat.php
+++ parts/postformat.php
@@ -5,9 +5,7 @@
 ?>
 
 <?php $format = get_post_format( $post_id ); ?>
-<?php if (is_sticky()) { ?>
-	<span>Featured</span> <i class="fa fa-heart"></i>
-<?php } else if ('video' == $format){ ?>
+<?php if ('video' == $format){ ?>
 	<span>Video</span> <i class="fa fa-youtube-play"></i>
 <?php } else if ('quote' == $format){ ?>
 	<span>Quote</span> <i class="fa fa-quote-right"></i>
