cb4830559f40b40da6ccbcf7ede499751a9b43c6
Paul <peiche@gmail.com>
2014-02-24 10:15:24 -0600

    Moved single post content back to content-single

--- single.php
+++ single.php
@@ -15,13 +15,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				$format = get_post_format();
-				if ( false === $format ) {
-					$format = 'standard';
-				}
-				?>
-				<?php get_template_part( 'content', $format ); ?>
+				<?php get_template_part( 'content', 'single' ); ?>
 
 				<?php cover_post_nav(); ?>
 
