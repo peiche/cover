1e3edaec04c31b88d3a3f9b10b588b2bad8c6679
Paul <peiche@gmail.com>
2014-02-24 10:20:11 -0600

    Added featured content support

---
+++ parts/cover-featured-post.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package Cover
+ */
+?>
+
+<div class="swiper-slide cover featured-image auto home">
+	<?php if ('' != get_the_post_thumbnail()) { ?>
+		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+	<?php } ?>
+	<div class="background"<?php if ('' != get_the_post_thumbnail()) { ?> style="background-image: url('<?php echo $image[0]; ?>');"<?php } ?>></div>
+	<header>
+		<h2>
+			<?php the_category(', ') ?>
+			
+			<span class="post-format pull-right">
+				<?php get_template_part( 'parts/postformat' ); ?>
+			</span>
+		</h2>
+		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<span>
+			<?php cover_posted_on(); ?>
+		</span>
+	</header>
+</div>
---
+++ parts/cover-featured.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Cover
+ */
+?>
+
+<div id="cover-home" class="swiper-container">
+	<div class="swiper-wrapper">
+		
+		<?php
+			/**
+			 * Fires before the Cover featured content.
+			 */
+			do_action( 'cover_featured_posts_before' );
+
+			$featured_posts = cover_get_featured_posts();
+			foreach ( (array) $featured_posts as $order => $post ) :
+				setup_postdata( $post );
+
+				 // Include the featured content template.
+				get_template_part( 'parts/cover', 'featured-post' );
+			endforeach;
+
+			/**
+			 * Fires after the Cover featured content.
+			 */
+			do_action( 'cover_featured_posts_after' );
+
+			wp_reset_postdata();
+		?>
+	</div>
+	
+	<?php if ( sizeof( $featured_posts ) > 1) { ?>
+		<span id="cover-home-left" class="fa fa-angle-left fa-fw"></span>
+		<span id="cover-home-right" class="fa fa-angle-right fa-fw"></span>
+	<?php } ?>
+	
+</div>
