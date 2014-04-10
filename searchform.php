d576a418b38c305693097a348b35e52a3902354d
Paul <peiche@gmail.com>
2014-04-10 13:29:07 -0500

    Updated search form text.

--- searchform.php
+++ searchform.php
@@ -1,7 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
-		<span class="screen-reader-text">Search for:</span>
-		<input type="search" class="search-field" placeholder="Search..." value="" name="s" title="Search for:" />
+		<span class="screen-reader-text">Search...</span>
+		<input type="search" class="search-field" placeholder="Search..." value="" name="s" title="Search..." />
 	</label>
-	<button class="btn btn-default" type="submit">Go</button>
 </form>
