710b8eb612ce1e4ecd336d92b677103740a158ee
Paul <peiche@gmail.com>
2014-04-21 13:21:17 -0500

    Revamp for smaller featured images

--- parts/cover-single.php
+++ parts/cover-single.php
@@ -5,22 +5,35 @@
 ?>
 
 <?php if ('' != get_the_post_thumbnail()) { ?>
-	<div class="cover featured-image">
-		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-		<div class="background" style="background-image: url('<?php echo $image[0]; ?>');" data-0-top="background-position: 50% 50%;" data-top-bottom="background-position: 50% 100%;"></div>
-		<header>
-			<h2>
-				<?php the_category(', ') ?>
-				
-				<span class="post-format pull-right">
-					<?php get_template_part( 'parts/postformat' ); ?>
+	
+	<?php
+		$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+		$width = $img[1]; // unused
+		$height = $img[2];
+		
+		$half = false;
+		if ($height <= 600) {
+			$half = true;
+		}
+	?>
+	
+	<div class="cover featured-image <?php if ( $half ) { echo 'half'; } ?>">
+		<div class="background" style="background-image: url('<?php echo $img[0]; ?>');" <?php if ( !$half ) { ?>data-0-top="background-position: 50% 50%;" data-top-bottom="background-position: 50% 100%;" <?php } ?> ></div>
+		<?php if ( !$half ) { ?>
+			<header>
+				<h2>
+					<?php the_category(', ') ?>
+					
+					<span class="post-format pull-right">
+						<?php get_template_part( 'parts/postformat' ); ?>
+					</span>
+				</h2>
+				<h1><a href="#"><?php the_title(); ?></a></h1>
+				<span>
+					<?php cover_posted_on(); ?>
 				</span>
-			</h2>
-			<h1><a href="#"><?php the_title(); ?></a></h1>
-			<span>
-				<?php cover_posted_on(); ?>
-			</span>
-		</header>
-		<i class="fa fa-angle-down"></i>
+			</header>
+			<i class="fa fa-angle-down"></i>
+		<?php } ?>
 	</div>
 <?php } ?>
