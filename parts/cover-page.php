30e205311dbad500cc2e25b165485fe93be4dc25
Paul <peiche@gmail.com>
2014-04-24 17:35:53 -0500

    Made the same cover changes on page as on single post.

--- parts/cover-page.php
+++ parts/cover-page.php
@@ -5,9 +5,20 @@
 ?>
 
 <?php if ('' != get_the_post_thumbnail()) { ?>
-	<div class="cover featured-image">
-		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-		<div class="background" style="background-image: url('<?php echo $image[0]; ?>');" <?php // data-top="background-position: 50% 50%;" data-top-bottom="background-position: 50% 100%;" ?> ></div>
+	
+	<?php
+		$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+		$width = $img[1]; // unused
+		$height = $img[2];
+		
+		$half = false;
+		if ($height <= 600) {
+			$half = true;
+		}
+	?>
+	
+	<div class="cover featured-image <?php if ( $half ) { echo 'half'; } ?>">
+		<div class="background" style="background-image: url('<?php echo $img[0]; ?>');"></div>
 		<header>
 			<?php if (is_page() && $post->post_parent) { ?>
 				<?php
