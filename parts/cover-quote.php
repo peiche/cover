4d7975a0e092315a5b147a7f65df2a4cd4ef1e92
Paul <peiche@gmail.com>
2014-04-28 09:05:35 -0500

    Stripped out cruft for quote post format header.

--- parts/cover-quote.php
+++ parts/cover-quote.php
@@ -6,18 +6,13 @@
 
 <?php
 	$has_thumbnail = false;
-	$half = false;
 	if ('' != get_the_post_thumbnail()) {
 		$has_thumbnail = true;
 		$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-		$height = $img[2];
-		if ($height <= 600) {
-			$half = true;
-		}
 	}
 ?>
 
-<div class="cover featured-image <?php if ($has_thumbnail && $half) { ?>half<?php } else if (!$has_thumbnail) { ?>auto<?php } ?>">
+<div class="cover featured-image auto">
 	<div class="background" <?php if ($has_thumbnail) { ?>style="background-image: url('<?php echo $img[0]; ?>');"<?php } ?>></div>
 	<header>
 		<h2><?php the_category(', ') ?></h2>
@@ -28,7 +23,4 @@
 		</h1>
 		<span><?php cover_posted_on(); ?></span>
 	</header>
-	<?php if ( $has_thumbnail && !$half ) { ?>
-		<i class="fa fa-angle-down"></i>
-	<?php } ?>
 </div>
