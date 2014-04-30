b79228d2987ae6a375812033f03a6f5d8ccc64a3
Paul <peiche@gmail.com>
2014-04-30 09:07:50 -0500

    Reverted a couple changes

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -83,7 +83,7 @@ function cover_posted_on() {
 	);
 
 	printf( __( '<span class="posted-on">%1$s', 'cover' ),
-		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%3$s">%2$s</a></span>',
+		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s">%2$s%3$s</a></span>',
 			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 			get_avatar( get_the_author_meta( 'ID' ), 35 ) . ' ',
 			esc_html( get_the_author() )
