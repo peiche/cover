1836e235011bedd575de1dee53c4fb25098d03d1
Paul <peiche@gmail.com>
2014-05-14 02:46:16 -0500

    Removed "half" cover

--- parts/cover-page.php
+++ parts/cover-page.php
@@ -8,16 +8,9 @@
 	
 	<?php
 		$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-		$width = $img[1]; // unused
-		$height = $img[2];
-		
-		$half = false;
-		if ($height <= 600) {
-			$half = true;
-		}
 	?>
 	
-	<div class="cover featured-image <?php if ( $half ) { echo 'half'; } ?>">
+	<div class="cover featured-image">
 		<div class="background" style="background-image: url('<?php echo $img[0]; ?>');"></div>
 		<header>
 			<?php if (is_page() && $post->post_parent) { ?>
--- parts/cover-single.php
+++ parts/cover-single.php
@@ -8,16 +8,9 @@
 	
 	<?php
 		$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-		$width = $img[1]; // unused
-		$height = $img[2];
-		
-		$half = false;
-		if ($height <= 600) {
-			$half = true;
-		}
 	?>
 	
-	<div class="cover featured-image <?php if ( $half ) { echo 'half'; } ?>">
+	<div class="cover featured-image">
 		<div class="background" style="background-image: url('<?php echo $img[0]; ?>');"></div>
 		<header>
 			<h1><?php the_title(); ?></h1>
@@ -25,8 +18,6 @@
 				<?php cover_posted_on(); ?>
 			</span>
 		</header>
-		<?php if ( !$half ) { ?>
-			<i class="fa fa-angle-down"></i>
-		<?php } ?>
+		<i class="fa fa-angle-down"></i>
 	</div>
 <?php } ?>
