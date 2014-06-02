4a0a91143d83d50d576acd9cdd74b7a901b710ef
Paul <peiche@gmail.com>
2014-06-02 17:48:07 -0500

    Added pagination for featured slider.
    Also removed code for related posts.

--- js/cover.js
+++ js/cover.js
@@ -33,7 +33,9 @@ jQuery(document).ready(function() {
 		homeSwiper = jQuery('#cover-home').swiper({
 			loop: true,
 			noSwiping: (homeSlides > 1 ? false : true),
-			simulateTouch: false
+			simulateTouch: false,
+			pagination: '.swiper-pagination',
+			paginationClickable: true
 		});
 		jQuery('#cover-home-left').click(function() {
 			homeSwiper.swipePrev();
@@ -50,22 +52,6 @@ jQuery(document).ready(function() {
 			scrollTop: top
 		}, 500);
 	});
-	
-	// related slider
-	if (jQuery('#related').length > 0) {
-		var relatedSlides = jQuery('#related .cover').length;
-		relatedSwiper = jQuery('#related').swiper({
-			loop: true,
-			noSwiping: (relatedSlides > 1 ? false : true),
-			simulateTouch: false
-		});
-		jQuery('#related-left').click(function() {
-			relatedSwiper.swipePrev();
-		});
-		jQuery('#related-right').click(function() {
-			relatedSwiper.swipeNext();
-		});
-	}
 	
 	// waypoints
     
