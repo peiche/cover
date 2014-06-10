39efe288a3dec9dc7da85f05753aa7c7adb4ffa5
Paul <peiche@gmail.com>
2014-06-10 10:16:15 -0500

    Grunt config using sass, uglify, and watch

---
+++ GruntFile.js
@@ -0,0 +1,53 @@
+/*!
+ * Cover-WP Gruntfile
+ * http://eichefam.net/projects/cover
+ * @author Paul Eiche
+ */
+ 
+'use strict';
+ 
+/**
+ * Grunt Module
+ */
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+            options: {
+                style: 'compressed',
+                noCache: true,
+				sourcemap: true,
+				unixNewlines: true
+            },
+			dist: {
+				files: {
+					'css/style.css' : 'sass/style.scss'
+				}
+			}
+		},
+        uglify: {
+            my_target: {
+                files: {
+                    'js/cover.min.js': ['src/cover.js'],
+                    'js/customizer.min.js': ['src/customizer.js'],
+                    'js/navigation.min.js': ['src/navigation.js'],
+                    'js/skip-link-focus-fix.min.js': ['src/skip-link-focus-fix.js']
+                }
+            }
+        },
+    	watch: {
+			css: {
+				files: 'sass/*.scss',
+				tasks: ['sass']
+			},
+            javascript: {
+                files: 'src/*.js',
+                tasks: ['uglify']
+            }
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
+}
