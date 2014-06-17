4f0d8b1ca09d165c9713d163cf14b7182a90b7a9
Paul <peiche@gmail.com>
2014-06-17 17:29:00 -0500

    Cover image changes
    Support for full, half, and minimal sizes

--- parts/cover-page.php
+++ parts/cover-page.php
@@ -8,9 +8,18 @@
 	
 	<?php
 		$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+		$width = $img[1]; // unused
+		$height = $img[2];
+
+        $class = '';
+        if ( $height <= 300 ) {
+            $class = 'auto';
+        } else if ($height <= 600) {
+            $class = 'half';
+        }
 	?>
 	
-	<div class="cover featured-image">
+	<div class="cover featured-image <?php echo $class; ?>">
 		<div class="background" style="background-image: url('<?php echo $img[0]; ?>');"></div>
 		<header>
 			<?php if (is_page() && $post->post_parent) { ?>
--- parts/cover-single.php
+++ parts/cover-single.php
@@ -11,13 +11,15 @@
 		$width = $img[1]; // unused
 		$height = $img[2];
 
-		$half = false;
-		if ($height <= 600) {
-			$half = true;
-		}
+        $class = '';
+        if ( $height <= 300 ) {
+            $class = 'auto';
+        } else if ($height <= 600) {
+            $class = 'half';
+        }
 	?>
 	
-	<div class="cover featured-image <?php if ( $half ) { echo 'half'; } ?>">
+	<div class="cover featured-image <?php echo $class; ?>">
 		<div class="background" style="background-image: url('<?php echo $img[0]; ?>');"></div>
 		<header>
 			<h1><?php the_title(); ?></h1>
@@ -25,7 +27,7 @@
 				<?php cover_posted_on(); ?>
 			</span>
 		</header>
-        <?php if ( !$half ) { ?>
+        <?php if ( $class == '' ) { ?>
 		  <i class="fa fa-angle-down"></i>
         <?php } ?>
 	</div>
