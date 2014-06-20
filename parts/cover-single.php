7b0f4e5ec7d3ffeefd654be038a35c732408aa92
Paul <peiche@gmail.com>
2014-06-20 17:38:32 -0500

    Cleaned up post cover logic

--- parts/cover-single.php
+++ parts/cover-single.php
@@ -4,31 +4,34 @@
  */
 ?>
 
-<?php if ('' != get_the_post_thumbnail()) { ?>
-	
-	<?php
-		$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-		$width = $img[1]; // unused
-		$height = $img[2];
+<?php
 
-        $class = '';
+    $class = '';
+
+    if ('' != get_the_post_thumbnail()) {
+        $img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+        $height = $img[2];
+
         if ( $height <= 300 ) {
             $class = 'auto';
-        } else if ($height <= 600) {
+        } else if ( $height <= 600 ) {
             $class = 'half';
         }
-	?>
-	
-	<div class="cover featured-image <?php echo $class; ?>">
-		<div class="background" style="background-image: url('<?php echo $img[0]; ?>');"></div>
-		<header>
-			<h1><?php the_title(); ?></h1>
-			<span>
-				<?php cover_posted_on(); ?>
-			</span>
-		</header>
-        <?php if ( $class == '' ) { ?>
-		  <i class="fa fa-angle-down"></i>
-        <?php } ?>
-	</div>
-<?php } ?>
+    } else {
+        $class = 'auto inverse';
+    }
+
+?>
+
+<div class="cover featured-image <?php echo $class; ?>">
+    <div class="background" <?php if ('' != get_the_post_thumbnail()) { ?>style="background-image: url('<?php echo $img[0]; ?>');"<?php } ?>></div>
+    <header>
+        <h1><?php the_title(); ?></h1>
+        <span>
+            <?php cover_posted_on(); ?>
+        </span>
+    </header>
+    <?php if ( $class == '' ) { ?>
+      <i class="fa fa-angle-down"></i>
+    <?php } ?>
+</div>
