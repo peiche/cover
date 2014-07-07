d9f5de3b0944433c408ff7f108925cd382cebcf7
Paul <peiche@gmail.com>
2014-07-07 17:32:51 -0500

    Moved post nav to bottom

--- single.php
+++ single.php
@@ -17,14 +17,14 @@ get_header(); ?>
 
                 <?php get_template_part( 'content', 'single' ); ?>
 
-                <?php cover_post_nav(); ?>
-
                 <?php
                     // If comments are open or we have at least one comment, load up the comment template
                     if ( comments_open() || '0' != get_comments_number() ) :
                         comments_template();
                     endif;
                 ?>
+                
+                <?php cover_post_nav(); ?>
 
             </main><!-- #main -->
         </div><!-- #primary -->
