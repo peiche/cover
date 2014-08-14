bfb67f33ac202013e6d122d3a0e842a3f5fbfaf1
Paul <peiche@gmail.com>
2014-08-14 02:01:29 -0500

    Retooled content to support grid view

--- content.php
+++ content.php
@@ -4,33 +4,38 @@
  */
 ?>
 
+<?php
+if ( has_post_thumbnail() ) {
+	$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+}
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
+	<a class="cover" href="<?php the_permalink(); ?>" rel="bookmark"></a>
+	
 	<?php if ( has_post_thumbnail() ) : ?>
-		<figure class="entry-image">
-			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail( 'single-post-thumbnail' ); ?></a>
-		</figure>
+		<?php if ('' != get_the_post_thumbnail()) { ?>
+			<div class="background" style="background-image: url('<?php echo $img[0]; ?>');"></div>
+		<?php } ?>
 	<?php endif; ?><!-- .entry-image -->
     
     <header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 	</header><!-- .entry-header -->
-
+	
 	<div class="entry-summary">
-		
-        <?php 
-        
-        $pos = strpos( $post->post_content, '<!--more-->' );
-        if ( $pos ) {
-            // the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'cover' ) );
-            the_content( __( '', 'cover' ) );
-        } else {
-            the_excerpt();
-        }
+		<?php 
 
-        ?>
-        
-        <?php
+		$pos = strpos( $post->post_content, '<!--more-->' );
+		if ( $pos ) {
+			the_content( __( '', 'cover' ) );
+		} else {
+			the_excerpt();
+		}
+
+		?>
+
+		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'cover' ),
 				'after'  => '</div>',
