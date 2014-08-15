79c82cc79871bfbe671873b116477b71a05598b6
Paul <peiche@gmail.com>
2014-08-15 17:47:33 -0500

    Grid markup changes.

--- index.php
+++ index.php
@@ -9,27 +9,36 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main grid" role="main">
-
+            
+            <?php
+                if ( cover_has_featured_posts() ) {
+                    get_template_part( 'parts/cover', 'featured' );
+                }
+            ?>
+            
 			<?php if ( have_posts() ) : ?>
-				<?php $count = 0; ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+				
+                <div class="grid">
+                
+                    <?php $count = 0; ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php // get_template_part( 'content' ); ?>
-					
-					<?php $count = $count + 1; ?>
-					<?php if ( $count < 2) { ?>
-						<div class="grid-row">
-					<?php } ?>
-					
-					<?php get_template_part( 'content' ); ?>
-					
-					<?php if ( $count == 2 ) { ?>
-						</div>
-						<?php $count = 0; ?>
-					<?php } ?>
-					
-				<?php endwhile; ?>
-				<?php cover_paging_nav(); ?>
+                        <?php $count = $count + 1; ?>
+                        <?php if ( $count < 2) { ?>
+                            <div class="grid-row">
+                        <?php } ?>
+
+                        <?php get_template_part( 'content' ); ?>
+
+                        <?php if ( $count == 2 ) { ?>
+                            </div>
+                            <?php $count = 0; ?>
+                        <?php } ?>
+
+                    <?php endwhile; ?>
+                    <?php cover_paging_nav(); ?>
+                    
+                </div>
 
 			<?php else : ?>
 
