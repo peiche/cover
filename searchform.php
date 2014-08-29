bb25f085cd9cf7c0d036070e4412495a1870c8f7
Paul <peiche@gmail.com>
2014-08-29 17:27:15 -0500

    Turned off autocomplete for search

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
 		<span class="screen-reader-text">Search...</span>
-		<input type="search" class="search-field" placeholder="Search..." value="<?php the_search_query(); ?>" name="s" title="Search" />
+		<input type="text" class="search-field" placeholder="Search..." value="<?php the_search_query(); ?>" name="s" title="Search" autocomplete="off" />
 	</label>
 </form>
