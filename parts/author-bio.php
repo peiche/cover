9b705d20cfebf380db22afb9ece027d823b04b83
Paul <peiche@gmail.com>
2014-09-19 17:33:51 -0500

    Pruning.

--- parts/author-bio.php
+++ parts/author-bio.php
@@ -19,39 +19,15 @@
 			<h4><?php echo get_the_author(); ?></h4>
 		<?php } ?>
 		
-		<?php if (!is_author()) { ?>
-			<a href="<?php echo the_author_meta('url'); ?>"><?php echo the_author_meta('url'); ?></a>
-		<?php } ?>
 		<p>
 			<?php the_author_meta( 'description' ); ?>
 		</p>
 		<?php if (!is_author()) { ?>
 			<p>
 				<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-					<?php printf( __( 'View all posts by %s <i class="fa fa-chevron-right"></i>', 'cover' ), get_the_author() ); ?>
+					<?php printf( __( 'View all posts by %s', 'cover' ), get_the_author() ); ?>
 				</a>
 			</p>
 		<?php } ?>
-		
-		<ul class="social">
-			
-			<?php
-				$fb = get_the_author_meta( 'facebook' );	// full facebook profile url
-				$tw = get_the_author_meta( 'twitter' );		// twitter username
-				$gp = get_the_author_meta( 'googleplus' );	// full g+ profile url
-				$jb = get_the_author_meta( 'jabber' );
-			?>
-			
-			<?php if ('' != $fb) { ?>
-				<li><a href="<?php echo $fb; ?>" target="_blank"></i></a></li>
-			<?php } ?>
-			<?php if ('' != $tw) { ?>
-				<li><a href="http://twitter.com/<?php echo $tw; ?>" target="_blank"></a></li>
-			<?php } ?>
-			<?php if ('' != $gp) { ?>
-				<li><a href="<?php echo $gp; ?>" target="_blank"></a></li>
-			<?php } ?>
-			
-		</ul>
 	</div>
 </div>
