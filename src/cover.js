73e4035369dceb12b457f1cd6e4d2d286c6e2576
Paul <peiche@gmail.com>
2014-10-07 17:43:00 -0500

    Trying to improve header behavior

--- src/cover.js
+++ src/cover.js
@@ -2,8 +2,7 @@ var homeSwiper, gallerySwiper, relatedSwiper;
 
 jQuery(document).ready(function() {
 	
-    // overlays
-    
+    // overlay
     jQuery('.toggle-overlay').click(function(e) {
         e.preventDefault();
         
@@ -19,12 +18,10 @@ jQuery(document).ready(function() {
         jQuery('.overlay').removeClass('show');
     });
     
-	// aesop
-	
+	// aesop stacked images have a default height of 500px, this matches them to window height
 	jQuery('.aesop-stacked-img').height(jQuery(window).height());
     
-    // back to top
-    
+    // clicking on header acts as "back to top" link
     jQuery('.header').click(function(e) {
         if (jQuery(e.target).closest('a').length == 0) {
             jQuery('html, body').animate({ scrollTop: jQuery('html').offset().top });
@@ -32,22 +29,22 @@ jQuery(document).ready(function() {
     });
     
     // waypoints
-    
     /*
     jQuery('.header').waypoint({
         handler: function(direction) {
             jQuery('.header').toggleClass('header--not-top');
-        }
     });
     */
     
-    // headroom
-    
-    var headroom = new Headroom(jQuery('.header')[0]);
+    // headroom init
+    var elem = jQuery('.header')[0];
+    var headroom = new Headroom(elem, {
+        tolerance: 5,
+        offset: jQuery('.cover').outerHeight()
+    });
     headroom.init();
     
-    // skrollr
-    
+    // skrollr init, if the user agent isn't a touch device (this is an imperfect solution)
     if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
 		skrollr.init({
 			forceHeight: false
@@ -55,11 +52,12 @@ jQuery(document).ready(function() {
 	}
     
     // add dropdown buttons to menus with children
-    
     jQuery('.menu .menu-item-has-children').append('<div class="sub-menu-toggle"><i class="fa fa-angle-down"></i></div>');
     
+    // hide submenus
     jQuery('.menu .menu-item-has-children .sub-menu').addClass('hide');
     
+    // click event on submenu toggles
     jQuery('.menu .menu-item-has-children').on('click', '.sub-menu-toggle', function() {
         var $this = jQuery(this);
         
