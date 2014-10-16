de84c247f5216a38ec8a4d50b3b2b53e2af0e421
Paul <peiche@gmail.com>
2014-10-16 17:50:04 -0500

    Updated for Sass 3.4

--- GruntFile.js
+++ GruntFile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
             options: {
                 style: 'compressed',
                 noCache: true,
-				sourcemap: false,
+				sourcemap: 'none',
 				unixNewlines: true
             },
 			dist: {
