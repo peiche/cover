66b0acf1266da45e4616a4645b53dce99629a1b5
Paul <peiche@gmail.com>
2014-10-17 03:03:50 -0500

    Changed post nav to allow all categories

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -45,8 +45,8 @@ if ( ! function_exists( 'cover_post_nav' ) ) :
  */
 function cover_post_nav() {
 	// Don't print empty markup if there's nowhere to navigate.
-	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( true, '', true );
-	$next = get_adjacent_post( true, '', false );
+	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+	$next = get_adjacent_post( false, '', false );
 
 	if ( ! $next && ! $previous ) {
 		return;
@@ -57,10 +57,10 @@ function cover_post_nav() {
 		<div class="nav-links cf">
 			<?php
 				$prev_img = wp_get_attachment_image_src( get_post_thumbnail_id( $previous->ID ), 'single-post-thumbnail' );
-				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<div class="post-nav-container"><div class="post-nav-background" style="background-image: url(\'' . $prev_img[0] . '\')"></div><i class="fa fa-fw fa-chevron-left meta-nav"></i></div><span class="left">%title</span>', 'Previous post link', 'cover' ), true );
+				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<div class="post-nav-container"><div class="post-nav-background" style="background-image: url(\'' . $prev_img[0] . '\')"></div><i class="fa fa-fw fa-chevron-left meta-nav"></i></div><span class="left">%title</span>', 'Previous post link', 'cover' ) );
 				
 				$next_img = wp_get_attachment_image_src( get_post_thumbnail_id( $next->ID ), 'single-post-thumbnail' );
-				next_post_link( '<div class="nav-next">%link</div>', _x( '<span class="right">%title</span><div class="post-nav-container"><div class="post-nav-background" style="background-image: url(\'' . $next_img[0] . '\')"></div><i class="fa fa-fw fa-chevron-right meta-nav"></i></div>', 'Next post link', 'cover' ), true );
+				next_post_link( '<div class="nav-next">%link</div>', _x( '<span class="right">%title</span><div class="post-nav-container"><div class="post-nav-background" style="background-image: url(\'' . $next_img[0] . '\')"></div><i class="fa fa-fw fa-chevron-right meta-nav"></i></div>', 'Next post link', 'cover' ) );
 			?>
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->
