f3c168e88da0f397a6dc929d8e8755626a92ed83
Paul <peiche@gmail.com>
2014-10-21 17:40:07 -0500

    Added site logo support
    In anticipation of Jetpack functionality

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -15,5 +15,9 @@ function cover_jetpack_setup() {
 		'container' => 'main',
 		'footer' => 'page'
 	) );
+    add_theme_support( 'site-logo', array( 
+        'cover-site-logo' 
+    ) );
+    add_theme_support( 'jetpack-responsive-videos' );
 }
 add_action( 'after_setup_theme', 'cover_jetpack_setup' );
