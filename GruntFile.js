644ccd092b92bde409794bdfb87bcb266cae7192
Paul <peiche@gmail.com>
2014-10-23 01:42:07 -0500

    Added a newline, because I can.

--- GruntFile.js
+++ GruntFile.js
@@ -48,5 +48,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['watch']);
+	
+	grunt.registerTask('default', ['watch']);
 }
