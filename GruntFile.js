b1e5edfe51ade80c49a5cf8aedc4e29793e3909e
Paul <peiche@gmail.com>
2014-10-23 17:38:11 -0500

    Added csslint and jshint for debugging

--- GruntFile.js
+++ GruntFile.js
@@ -12,19 +12,76 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-            options: {
-                style: 'compressed',
-                noCache: true,
-				sourcemap: 'none',
-				unixNewlines: true
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    noCache: true,
+                    sourcemap: 'none',
+                    unixNewlines: true
+                },
+                files: {
+                    'style.css': 'sass/style.scss'
+                }
             },
-			dist: {
-				files: {
-					'style.css' : 'sass/style.scss'
-				}
-			}
+            dist: {
+                options: {
+                    style: 'compressed',
+                    noCache: true,
+                    sourcemap: 'none',
+                    unixNewlines: true
+                },
+                files: {
+                    'style.min.css': 'sass/style.scss'
+                }
+            },
 		},
+        csslint: {
+            options: {
+                formatters: [
+                    {
+                        id: 'text',
+                        dest: 'report/csslint.txt'
+                    }
+                ]
+            },
+            strict: {
+                options: {
+                    import: 2,
+                    'box-sizing': false,
+                    'adjoining-classes': false
+                },
+                src: ['style.css']
+            }
+        },
+        jshint: {
+            options: {
+                "bitwise": true,
+                "browser": true,
+                "curly": true,
+                "eqeqeq": true,
+                "eqnull": true,
+                "es5": true,
+                "esnext": true,
+                "immed": true,
+                "jquery": true,
+                "latedef": true,
+                "newcap": true,
+                "noarg": true,
+                "node": true,
+                "strict": false,
+                "trailing": true,
+                "undef": true,
+                "globals": {
+                "jQuery": true,
+                    "alert": true
+                }
+            },
+            all: [
+                'Gruntfile.js',
+                'src/cover.js'
+            ]
+        },
         uglify: {
             my_target: {
                 files: {
@@ -34,20 +91,24 @@ module.exports = function(grunt) {
                 }
             }
         },
-    	watch: {
+        watch: {
 			css: {
 				files: 'sass/*.scss',
-				tasks: ['sass']
+				tasks: ['sass', 'csslint']
 			},
             javascript: {
                 files: 'src/*.js',
-                tasks: ['uglify']
+                tasks: ['uglify'] // FIXME: add jshint
             }
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
+    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
-	
-	grunt.registerTask('default', ['watch']);
-}
+    
+	grunt.registerTask('sasslint', ['sass', 'csslint']); // FIXME: add jshint
+    grunt.registerTask('default', ['sass', 'csslint', 'uglify', 'watch']); // FIXME: add jshint
+};
