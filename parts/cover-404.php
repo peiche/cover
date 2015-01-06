3fb2aa47718ae21adc35d1de784110b31f903e78
Paul <peiche@gmail.com>
2015-01-06 02:58:17 -0600

    Class changes for 404 cover

--- parts/cover-404.php
+++ parts/cover-404.php
@@ -4,8 +4,7 @@
  */
 ?>
 
-<div class="cover featured-image auto">
-	<div class="background<?php if ( '' != $image ) { ?> darken" style="background-image: url('<?php echo $image[0]; ?>');<?php } ?>" data-0-top="background-position: 50% 50%;" data-top-bottom="background-position: 50% 100%;"></div>
+<div class="cover">
 	<header class="cover-header">
 		<h1 class="cover-title">404</h1>
 		<p class="align-center">It looks like nothing was found at this location. Maybe try one of the links below or a search?</p>
