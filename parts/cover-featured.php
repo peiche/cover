44f5f2477cf8e3e2a1f704f87ddbaa1bf437b35d
Paul <peiche@gmail.com>
2015-01-06 02:56:56 -0600

    Changes to featured content area

--- parts/cover-featured.php
+++ parts/cover-featured.php
@@ -4,52 +4,53 @@
  */
 ?>
 
-<?php
-    /**
-     * Fires before the Cover featured content.
-     */
-    do_action( 'cover_featured_posts_before' );
+<div class="featured-container">
 
-    $featured_posts = cover_get_featured_posts();
-    foreach ( (array) $featured_posts as $order => $post ) :
-        setup_postdata( $post );
-?>
+	<?php
+		/**
+		 * Fires before the Cover featured content.
+		 */
+		do_action( 'cover_featured_posts_before' );
 
-<?php
-    if ( '' != get_the_post_thumbnail() ) {
-        $img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
-    }
-?>
+		$featured_posts = cover_get_featured_posts();
+		foreach ( (array) $featured_posts as $order => $post ) :
+			setup_postdata( $post );
 
-<div class="cover">
-    <div class="background"<?php if ( '' != $img ) { ?> style="background-image: url('<?php echo $img; ?>');"<?php } ?>></div>
-    
-    <header class="cover-header">
+			if ( '' != get_the_post_thumbnail() ) {
+				$img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
+			}
+	?>
 
-        <h2 class="cover-subtitle">Featured</h2>
-        
-        <?php if ( is_page() ) { ?>
-            <?php if ( $post->post_parent ) {
-                $parent_permalink = get_permalink($post->post_parent);
-                $parent_title = get_the_title($post->post_parent);
-            ?>
-                <h2 class="cover-subtitle"><a href="<?php echo $parent_permalink; ?>"><i class="fa fa-angle-left"></i> <?php echo $parent_title; ?></a></h2>
-            <?php } ?>
-        <?php } ?>
+	
+		<a href="<?php the_permalink(); ?>" rel="bookmark">
+			<div class="cover<?php if ( '' != $img ) { ?> featured-image<?php } ?>">
+				<div class="background"<?php if ( '' != $img ) { ?> style="background-image: url('<?php echo $img; ?>');"<?php } ?>></div>
+				<header class="cover-header">
+					<h1 class="cover-title"><?php the_title(); ?></h1>
+					<h2 class="cover-subtitle">
+					<?php 
+						$pos = strpos( $post->post_content, '<!--more-->' );
+						if ( $pos ) {
+							echo get_the_content( __( '', 'cover' ) );
+						} else {
+							echo get_the_excerpt();
+						}
+					?>
+					</h2>
+				</header>
+			</div>
+		</a>
+	
 
-        <h1 class="cover-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+	<?php
+		endforeach;
 
-    </header>
-    
+		/**
+		 * Fires after the Cover featured content.
+		 */
+		do_action( 'cover_featured_posts_after' );
+
+		wp_reset_postdata();
+	?>
+
 </div>
-
-<?php
-    endforeach;
-
-    /**
-     * Fires after the Cover featured content.
-     */
-    do_action( 'cover_featured_posts_after' );
-
-    wp_reset_postdata();
-?>
