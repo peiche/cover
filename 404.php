56274c2cff0e546e8b183891afcebf0a979c29e0
Paul <peiche@gmail.com>
2015-01-06 02:25:16 -0600

    Moved cover include inside wrapper

--- 404.php
+++ 404.php
@@ -7,11 +7,12 @@
 
 get_header(); ?>
 
-<?php get_template_part( 'parts/cover', '404' ); ?>
 <?php get_template_part( 'parts/wrapper', 'top' ); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			
+			<?php get_template_part( 'parts/cover', '404' ); ?>
 
 			<section class="error-404 not-found">
 				<div class="page-content">
