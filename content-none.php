200e609f5faf9507d028a8a31dc5bbc1b5c34e3a
Paul <peiche@gmail.com>
2015-01-06 02:26:02 -0600

    Removed header

--- content-none.php
+++ content-none.php
@@ -10,10 +10,6 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', '_s' ); ?></h1>
-	</header><!-- .page-header -->
-
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
@@ -22,7 +18,7 @@
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '_s' ); ?></p>
-			
+
 		<?php else : ?>
 
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', '_s' ); ?></p>
