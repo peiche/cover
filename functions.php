910bb66c25963397bcfaa6787b3d05a91141077e
Paul <peiche@gmail.com>
2015-01-07 01:55:43 -0600

    Added color contrast functions.

--- functions.php
+++ functions.php
@@ -30,9 +30,7 @@ function cover_setup() {
 	 */
 	load_theme_textdomain( 'cover', get_template_directory() . '/languages' );
     
-    add_image_size( 'cover-site-logo', '300', '300' );
-    
-	// Add default posts and comments RSS feed links to head.
+    // Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
 	/*
@@ -52,7 +50,7 @@ function cover_setup() {
 
 	// Enable support for HTML5 markup.
 	add_theme_support( 'html5', array( 'comment-list', 'search-form', 'comment-form', ) );
-    
+	
 	// Enable featured content.
     add_theme_support( 'featured-content', array(
 		'filter'		=> 'cover_get_featured_posts',
@@ -106,7 +104,7 @@ function cover_scripts() {
 	wp_enqueue_script( 'cover-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 	wp_enqueue_script( 'skrollr', get_template_directory_uri() . '/js/skrollr.js', array(), '20140821', true );
     wp_enqueue_script( 'headroom', get_template_directory_uri() . '/js/headroom.min.js', array(), '20140814', true );
-    wp_enqueue_script( 'cover-lib', get_template_directory_uri() . '/js/cover.min.js', array(), '20140210', true );
+	wp_enqueue_script( 'cover-lib', get_template_directory_uri() . '/js/cover.min.js', array(), '20140210', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -114,10 +112,51 @@ function cover_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'cover_scripts' );
 
-/**
- * Implement the Custom Header feature.
+/*
+ * Color contrast calculations.
+ * @link http://24ways.org/2010/calculating-color-contrast/
  */
-require get_template_directory() . '/inc/custom-header.php';
+function getContrast50( $hexcolor ) {
+    return ( hexdec( $hexcolor ) > 0xffffff / 2 ) ? 'light' : 'dark';
+}
+function getContrastYIQ( $hexcolor ) {
+	$r = hexdec( substr( $hexcolor, 0, 2 ) );
+	$g = hexdec( substr( $hexcolor, 2, 2 ) );
+	$b = hexdec( substr( $hexcolor, 4, 2 ) );
+	$yiq = ( ( $r * 299 ) + ( $g * 587 ) + ( $b * 114 ) ) / 1000;
+	
+    return ( $yiq >= 128 ) ? 'light' : 'dark';
+}
+/*
+ * Leave the function that you want uncommented. 
+ */
+function getContrast( $hexcolor ) {
+    return 
+        //getContrast50( $hexcolor )
+        getContrastYIQ( $hexcolor )
+        ;
+}
+
+/*
+ * Utility function to convert a hexidecimal color to rgb.
+ * @link http://css-tricks.com/snippets/php/convert-hex-to-rgb/
+ */
+function hex2rgb( $colour ) {
+        if ( $colour[0] == '#' ) {
+                $colour = substr( $colour, 1 );
+        }
+        if ( strlen( $colour ) == 6 ) {
+                list( $r, $g, $b ) = array( $colour[0] . $colour[1], $colour[2] . $colour[3], $colour[4] . $colour[5] );
+        } elseif ( strlen( $colour ) == 3 ) {
+                list( $r, $g, $b ) = array( $colour[0] . $colour[0], $colour[1] . $colour[1], $colour[2] . $colour[2] );
+        } else {
+                return false;
+        }
+        $r = hexdec( $r );
+        $g = hexdec( $g );
+        $b = hexdec( $b );
+        return array( 'red' => $r, 'green' => $g, 'blue' => $b );
+}
 
 /**
  * Custom template tags for this theme.
