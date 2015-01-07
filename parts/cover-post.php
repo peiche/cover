ab81128924d4025fb86bb89d03173c85b4d766c6
Paul <peiche@gmail.com>
2015-01-07 01:56:32 -0600

    Markup changes for post and page cover.

--- parts/cover-post.php
+++ parts/cover-post.php
@@ -12,30 +12,22 @@
         $img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
         $width = $img[1];
         $height = $img[2];
-		
+
 		$class = ' featured-image';
-		
-        if ( $height <= 600 ) {
-            $class = $class . ' auto';
-        }
+
+        if ( $height > 1 && $height <= 600 ) {
+            $class = $class . ' hero';
+        } else if ( $height > 600 ) {
+			$class = $class . ' full';
+		}
     }
 
 ?>
 
 <div class="cover<?php echo $class; ?>">
-    <div class="background" <?php if ( '' != $class ) { ?>style="background-image: url('<?php echo $img[0]; ?>');"<?php } ?> data-0-top="background-position: 50% 50%;" data-top-bottom="background-position: 50% 100%;"></div>
-    
-    <header class="cover-header">
+    <div class="background" <?php if ( '' != $class ) { ?>style="background-image: url('<?php echo $img[0]; ?>');"<?php } ?>></div>
 
-        <?php if ( is_single() ) { ?>
-            <?php
-                /* translators: used between list items, there is a space after the comma */
-                $categories_list = get_the_category_list( __( ', ', 'cover' ) );
-                if ( $categories_list && cover_categorized_blog() ) :
-            ?>
-                <h2 class="cover-subtitle"><?php echo $categories_list; ?></h2>
-            <?php endif; ?>
-        <?php } ?>
+    <header class="cover-header">
 
         <?php if ( is_page() ) { ?>
             <?php if ( $post->post_parent ) {
@@ -46,8 +38,8 @@
             <?php } ?>
         <?php } ?>
 
-        <h1 class="cover-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+        <h1 class="cover-title"><?php the_title(); ?></h1>
 
     </header>
-    
+
 </div>
