b2b80e4e52734ac99451dbc2136b8a80f3c88b6d
Paul <peiche@gmail.com>
2015-01-19 13:42:16 -0600

    Removed options.

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,80 +14,11 @@ function cover_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
     $wp_customize->remove_section( 'colors' );
-	
-    $wp_customize->add_section( 'cover_options', array(
-        'title' 		=> __( 'Cover Theme Options', 'cover' )
-    ) );
-    
-    $wp_customize->add_setting( 'cover_header_background_color', array(
-        'default' 		=> '#333333',
-        'type' 			=> 'option'
-    ) );
-    $wp_customize->add_control( 
-        new WP_Customize_Color_Control( 
-        $wp_customize, 
-        'cover_header_background_color', 
-        array(
-            'label'      => __( 'Header Background Color', 'cover' ),
-            'section'    => 'cover_options',
-            'settings'   => 'cover_header_background_color',
-        ) ) 
-    );
-    
-    $wp_customize->add_setting( 'cover_header_text_color', array(
-        'default' 		=> '#ffffff',
-        'type' 			=> 'option'
-    ) );
-    $wp_customize->add_control( 
-        new WP_Customize_Color_Control( 
-        $wp_customize, 
-        'cover_header_text_color', 
-        array(
-            'label'      => __( 'Header Text Color', 'cover' ),
-            'section'    => 'cover_options',
-            'settings'   => 'cover_header_text_color',
-        ) ) 
-    );
 }
 add_action( 'customize_register', 'cover_customize_register' );
 
 function cover_customize_options() {
-    $header_background_color = get_option( 'cover_header_background_color' );
-    $header_text_color = get_option( 'cover_header_text_color' );
-    $header_text_color_rgb = hex2rgb( $header_text_color );
-    ?>
-    
-    <style>
-    .header .backdrop,
-    .cover {
-        background-color: <?php echo $header_background_color; ?> !important; /* remove !important once custom css is back in stylesheet */
-    }
-    
-    .header a,
-    .header .site-description {
-        color: <?php echo $header_text_color; ?>;
-    }
-    
-    .header a:hover,
-    .header .author:hover .name {
-        border-color: <?php echo $header_text_color; ?>;
-    }
-    
-    .header .site-description {
-        border-color: rgba(<?php echo $header_text_color_rgb[ 'red' ]; ?>, <?php echo $header_text_color_rgb[ 'green' ]; ?>, <?php echo $header_text_color_rgb[ 'blue' ]; ?>, .25);
-    }
-    
-    .hamburger span,
-    .hamburger span:before,
-    .hamburger span:after {
-        background-color: <?php echo $header_text_color; ?>;
-    }
-    
-    </style>
-
-    <meta name="theme-color" content="<?php echo $header_color; ?>">
-    
-    <?php
+    //
 }
 add_action( 'wp_head', 'cover_customize_options' );
 
