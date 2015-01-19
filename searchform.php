4a3bb67bf019033b1953d59e1aa80c3cbf33a821
Paul <peiche@gmail.com>
2015-01-19 01:08:11 -0600

    Removed search value from form field

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
 		<span class="screen-reader-text">Search...</span>
-		<input type="text" class="search-field" placeholder="Search..." value="<?php the_search_query(); ?>" name="s" title="Search" autocomplete="off" />
+		<input type="text" class="search-field" placeholder="Search..." name="s" title="Search" autocomplete="off" />
 	</label>
 </form>
