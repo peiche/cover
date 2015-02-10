f015d2c884f26d019f235fb3f91c024ad77755cd
Paul <peiche@gmail.com>
2015-02-10 09:36:44 -0600

    Fixes for 404 page.

--- 404.php
+++ 404.php
@@ -17,33 +17,8 @@ get_header(); ?>
 			<section class="error-404 not-found">
 				<div class="page-content">
 					
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( cover_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'cover' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 0,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'cover' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
+                    <p>It looks like nothing was found at this location. Maybe try a search?</p>
+                    
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
--- inc/cover-404.php
+++ inc/cover-404.php
@@ -7,7 +7,5 @@
 <div class="cover">
 	<header class="cover-header">
 		<h1 class="cover-title">404</h1>
-		<p class="align-center">It looks like nothing was found at this location. Maybe try one of the links below or a search?</p>
-		<?php get_search_form(); ?>
 	</header>
 </div>
