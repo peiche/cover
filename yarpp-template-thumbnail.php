28b4b5a08458d683e20eed70d6dfc493347a060f
Paul <peiche@gmail.com>
2015-02-11 12:41:21 -0600

    Finalized yarpp template.

--- yarpp-template-thumbnail.php
+++ yarpp-template-thumbnail.php
@@ -1,14 +1,15 @@
 <?php
 /*
 YARPP Template: Cover
-Description: Full-width related posts template built for the Cover theme.
+Description: Full-width related posts template built for the Cover theme. Works best with a maximum number divisible by 4.
 Author: Paul Eiche
 */ ?>
 <?php if (have_posts()):?>
+
 <h2>Related</h2>
 <ul class="yarpp-related-cover cf">
-    <?php while (have_posts()) : the_post(); ?>
     
+    <?php while (have_posts()) : the_post(); ?>
     <li>
         <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
             <?php if (has_post_thumbnail()):?>
@@ -19,16 +20,6 @@ Author: Paul Eiche
     </li>
     <?php endwhile; ?>
     
-    <?php // show random posts
-    /*
-    query_posts("orderby=rand&order=asc&limit=1");
-the_post();?>
-<p>No related posts were found, so here's a consolation prize: <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>.</p>
-    */
-    ?>
-    
 </ul>
 
-<?php else: ?>
-<!--<p>No related photos.</p>-->
 <?php endif; ?>
