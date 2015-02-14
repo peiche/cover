227cdac50a8b9bbc7ee963cc403b1f37c6399888
Paul <peiche@gmail.com>
2015-02-14 01:53:43 -0600

    Fixed php syntax

--- inc/author-bio.php
+++ inc/author-bio.php
@@ -14,7 +14,7 @@
 	</div>
 	
     <div class="profile-info">
-        <?php if (is_author()) { ?>
+        <?php if ( is_author() ) { ?>
             <h1 class="cover-title"><?php echo get_the_author(); ?></h1>
         <?php } else { ?>
             <h4><a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php echo get_the_author(); ?></a></h4>
