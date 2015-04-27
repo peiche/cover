841638e794d7d7e97dc669a92c6b456100275b9e
Paul <peiche@gmail.com>
2015-04-27 16:48:16 -0500

    Fixed broken translate string.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
 		<span class="screen-reader-text">Search...</span>
-		<input type="text" class="search-field" placeholder="<?php __( 'Search...', 'cover' ) ?>" name="s" title="Search" autocomplete="off" />
+		<input type="text" class="search-field" placeholder="<?php _e( 'Search...', 'cover' ) ?>" name="s" title="Search" autocomplete="off" />
 	</label>
 </form>
