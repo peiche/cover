90daf469c56cbb1b02d82c3e7e5140e29d1b27a1
Paul <peiche@gmail.com>
2015-04-27 01:06:20 -0500

    Normalized zip task name.

--- GruntFile.js
+++ GruntFile.js
@@ -125,7 +125,7 @@ module.exports = function(grunt) {
             }
 		},
         compress: {
-            main: {
+            build: {
                 options: {
                     mode: 'zip',
                     archive: function() {
