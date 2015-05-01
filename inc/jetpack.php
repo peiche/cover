03d14a9a03d26a15b2c60e909b52636efd662e01
Paul <peiche@gmail.com>
2015-05-01 17:27:15 -0500

    In-flight changes for improving infinite scroll

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -15,21 +15,41 @@ function cover_jetpack_setup() {
      * See Jetpack support for more info
      * @link http://jetpack.me/support/infinite-scroll/
      */
-	add_theme_support( 'infinite-scroll', array(
-        'type' => 'click',
+    add_theme_support( 'infinite-scroll', array(
+        'type' => 'scroll',
+        'footer_widgets' => false,
         'container' => 'main',
 		'footer' => false,
 	) );
 
+    // link?
     add_theme_support( 'jetpack-responsive-videos' );
 
-    // Enable featured content.
+    // Enable featured content. link?
     add_theme_support( 'featured-content', array(
 		'filter'		=> 'cover_get_featured_posts',
         'max_posts'     => 1,
 	) );
 }
 add_action( 'after_setup_theme', 'cover_jetpack_setup' );
+
+/**
+ * Handle `footer_widgets` argument
+ *
+ * @param bool $has_widgets
+ * @uses has_nav_menu
+ * @filter infinite_scroll_has_footer_widgets
+ * @return bool
+ */
+function cover_infinite_scroll_has_footer_widgets( $has_widgets ) {
+    $has_widgets = false;
+    if ( has_nav_menu( 'social_footer' ) ) {
+        $has_widgets = true;
+    }
+    
+    return $has_widgets;
+}
+add_filter( 'infinite_scroll_has_footer_widgets', 'cover_infinite_scroll_has_footer_widgets', 10, 1 );
 
 /**
  * Getter function for Featured Content Plugin.
