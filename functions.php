e111ac6e801396a5ddf56a69bf8f2899b2114a9f
Paul <peiche@gmail.com>
2015-05-20 17:47:56 -0500

    Added function to alter color
    similar to how Sass lighten() and darken() work

--- functions.php
+++ functions.php
@@ -144,6 +144,75 @@ function hex2rgb( $colour ) {
 }
 
 /**
+ * Alter color brightness
+ * @link http://jaspreetchahal.org/how-to-lighten-or-darken-hex-or-rgb-color-in-php-and-javascript/
+ * @param $color_code
+ * @param int $percentage_adjuster
+ * @return array|string
+ * @author Jaspreet Chahal
+ */
+function adjustColorLightenDarken($color_code,$percentage_adjuster = 0) {
+    $percentage_adjuster = round($percentage_adjuster/100,2);
+    if(is_array($color_code)) {
+        $r = $color_code["r"] - (round($color_code["r"])*$percentage_adjuster);
+        $g = $color_code["g"] - (round($color_code["g"])*$percentage_adjuster);
+        $b = $color_code["b"] - (round($color_code["b"])*$percentage_adjuster);
+ 
+        return array("r"=> round(max(0,min(255,$r))),
+            "g"=> round(max(0,min(255,$g))),
+            "b"=> round(max(0,min(255,$b))));
+    }
+    else if(preg_match("/#/",$color_code)) {
+        $hex = str_replace("#","",$color_code);
+        $r = (strlen($hex) == 3)? hexdec(substr($hex,0,1).substr($hex,0,1)):hexdec(substr($hex,0,2));
+        $g = (strlen($hex) == 3)? hexdec(substr($hex,1,1).substr($hex,1,1)):hexdec(substr($hex,2,2));
+        $b = (strlen($hex) == 3)? hexdec(substr($hex,2,1).substr($hex,2,1)):hexdec(substr($hex,4,2));
+        $r = round($r - ($r*$percentage_adjuster));
+        $g = round($g - ($g*$percentage_adjuster));
+        $b = round($b - ($b*$percentage_adjuster));
+ 
+        return "#".str_pad(dechex( max(0,min(255,$r)) ),2,"0",STR_PAD_LEFT)
+            .str_pad(dechex( max(0,min(255,$g)) ),2,"0",STR_PAD_LEFT)
+            .str_pad(dechex( max(0,min(255,$b)) ),2,"0",STR_PAD_LEFT);
+ 
+    }
+}
+
+/*
+
+USAGE:
+
+// lightening colors
+print_r(adjustColorLightenDarken(array("r"=>204,
+                                "g"=>136,
+                                "b"=>0),-5));
+//outputs 
+//Array
+//(
+//    [r] => 214
+//    [g] => 143
+//    [b] => 0
+//)
+print_r(adjustColorLightenDarken("#C80",-5));
+// outputs #d68f00
+
+// darkening colors
+print_r(adjustColorLightenDarken(array("r"=>204,
+                                "g"=>136,
+                                "b"=>0),5));
+//outputs 
+//Array
+//(
+//    [r] => 194
+//    [g] => 129
+//    [b] => 0
+//)
+print_r(adjustColorLightenDarken("#C80",5));
+// outputs #c28100
+
+*/
+
+/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
