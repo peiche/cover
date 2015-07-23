3ab4ad9f154a4bb2c666374710098c95ecf32b7e
Paul <peiche@gmail.com>
2015-07-23 17:42:52 -0500

    Tweaked profile layout

--- inc/author-bio.php
+++ inc/author-bio.php
@@ -13,15 +13,15 @@
 	<div class="profile-avatar">
 		<?php echo get_avatar( get_the_author_meta( 'ID' ), ( is_author() ? 120 : 80 ) ); ?>
 	</div>
-	
-    <div class="profile-info">
-        <?php if ( is_author() ) { ?>
-            <h1 class="cover-title"><?php echo get_the_author(); ?></h1>
-        <?php } else { ?>
-            <h4><a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php echo get_the_author(); ?></a></h4>
-        <?php } ?>
-        <p>
-            <?php the_author_meta( 'description' ); ?>
-        </p>
-    </div>
+
+  <div class="profile-info">
+    <?php if ( is_author() ) { ?>
+      <h1 class="cover-title"><?php echo get_the_author(); ?></h1>
+    <?php } else { ?>
+      <h4><a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php echo get_the_author(); ?></a></h4>
+    <?php } ?>
+  </div>
+	<div class="profile-bio">
+		<?php the_author_meta( 'description' ); ?>
+	</div>
 </div>
