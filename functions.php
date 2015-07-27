8a88ba9583b67247773e6489396ea10c278f60d3
Paul <peiche@gmail.com>
2015-07-27 17:35:05 -0500

    Added unslider for featured content

--- functions.php
+++ functions.php
@@ -88,7 +88,8 @@ function cover_scripts() {
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'cover-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 	wp_enqueue_script( 'skrollr', get_template_directory_uri() . '/js/skrollr.min.js', array(), '20140821', true );
-    wp_enqueue_script( 'headroom', get_template_directory_uri() . '/js/headroom.min.js', array(), '20140814', true );
+  wp_enqueue_script( 'headroom', get_template_directory_uri() . '/js/headroom.min.js', array(), '20140814', true );
+	wp_enqueue_script( 'unslider', get_template_directory_uri() . '/js/unslider.min.js', array(), '20150727', true );
 	wp_enqueue_script( 'cover-lib', get_template_directory_uri() . '/js/cover.min.js', array(), '20140210', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
