e4131bb146b6a574efbec477172f5d1f1940bb5d
Paul <peiche@gmail.com>
2015-07-31 02:41:15 -0500

    Fix for masonry

--- assets/js/cover-masonry.js
+++ assets/js/cover-masonry.js
@@ -4,28 +4,16 @@ jQuery(document).ready(function() {
    * Masonry grid for items in blog or archive view.
    **/
 
-  // init
-  var $container = jQuery('.site-main .grid');
-  $container.masonry({
-    itemSelector: '.hentry', // set itemSelector so .grid-sizer is not used in layout
-    columnWidth: '.hentry', // use element for option
+  var $grid = jQuery('.site-main .grid');
+  $grid.masonry({
+    itemSelector: '.hentry',
+    columnWidth: '.hentry',
     percentPosition: true
   });
 
   var infinite_count = 0;
-  jQuery( document.body ).on( 'post-load', function () {
-    infinite_count = infinite_count + 1;
-
-    var $selector = jQuery('#infinite-view-' + infinite_count);
-    var $elements = $selector.find('.hentry');
-    //$elements.hide();
-
-    // ??
-    $container.append($elements).masonry('appended', $elements);
-    //$selector.hide();
-    //$elements.fadeIn();
-
-    //$container.trigger('masonry-items-added');
+  jQuery( document.body ).on('post-load', function () {
+    var $elements = jQuery('.infinite-wrap').find('.hentry');
+    $grid.append($elements).masonry('appended', $elements);
   });
-
 });
