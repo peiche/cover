35b07240764e6a9c0327c516aa3728668bfba984
Paul <peiche@gmail.com>
2015-08-31 12:15:28 -0500

    Nitpicky edit.

--- assets/js/cover-masonry.js
+++ assets/js/cover-masonry.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function() {
   });
 
   var infinite_count = 0;
-  jQuery( document.body ).on('post-load', function () {
+  jQuery(document.body).on('post-load', function () {
     var $elements = jQuery('.infinite-wrap').find('.hentry');
     $grid.append($elements).masonry('appended', $elements);
   });
