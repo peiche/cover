c76b180df9566f6e409b6fa945a2201ef0bec5fd
Paul <peiche@gmail.com>
2015-09-15 12:41:13 -0500

    Fixed theme color used by Chrome in Android Lollipop

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="theme-color" content="#026ed2">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
