311e1594efd4f2e6d989db354b0cefd2fde523d7
Paul <peiche@gmail.com>
2016-01-15 12:51:55 -0600

    Removed chapter scroll offset

--- inc/aesop.php
+++ inc/aesop.php
@@ -4,12 +4,3 @@
  *
  * @package Cover
  */
-
-add_filter( 'aesop_chapter_scroll_offset', 'cover_chapter_scroll_offset' );
-
-/**
- * Set the offset for chapter scrolling to match the header height.
- */
-function cover_chapter_scroll_offset() {
-    return 76;
-}
