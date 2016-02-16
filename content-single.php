541c025b738a71465ee31d72c3d4ac1e4402396c
Paul <peiche@gmail.com>
2016-02-16 16:11:15 -0600

    Added category listing to post footer

--- content-single.php
+++ content-single.php
@@ -22,19 +22,32 @@
 	<footer class="entry-meta">
 
 		<?php
+			$categories = get_the_category();
+			if ( $categories && cover_categorized_blog() ) {
+		?>
+      <ul class="categories">
+	      <?php
+	        foreach ( $categories as $category ) {
+	    			echo '<li><a href="' . get_tag_link( $category->term_id ) . '" title="' . esc_attr( sprintf( __( 'View all posts in %s', 'cover' ), $category->name ) ) . '">' . $category->name . '</a></li>';
+	        }
+	      ?>
+      </ul>
+    <?php } ?>
+
+		<?php
 			$tags = get_the_tags();
 			if ( $tags ) {
 		?>
-        <ul class="tags">
-        <?php
-                foreach ( $tags as $tag ) {
-            		echo '<li><a href="' . get_tag_link( $tag->term_id ) . '" title="' . esc_attr( sprintf( __( 'View all posts in %s', 'cover' ), $tag->name ) ) . '">' . $tag->name . '</a></li>';
-                }
-        ?>
-        </ul>
-        <?php } ?>
+      <ul class="tags">
+	      <?php
+	        foreach ( $tags as $tag ) {
+	    			echo '<li><a href="' . get_tag_link( $tag->term_id ) . '" title="' . esc_attr( sprintf( __( 'View all posts in %s', 'cover' ), $tag->name ) ) . '">' . $tag->name . '</a></li>';
+	        }
+	      ?>
+      </ul>
+    <?php } ?>
 
-        <?php edit_post_link( __( '<i class="fa fa-pencil"></i> Edit', 'cover' ), '<div><span class="edit-link">', '</span></div>' ); ?>
+    <?php edit_post_link( __( '<i class="fa fa-pencil"></i> Edit', 'cover' ), '<div><span class="edit-link">', '</span></div>' ); ?>
 
 		<?php if ( 'thread' != get_post_type() ) { ?>
 			<?php get_template_part( 'inc/author-bio' ); ?>
