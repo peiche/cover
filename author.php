6f8b585f4d95534bd21d0fe491159f08ceb6e6eb
Paul <peiche@gmail.com>
2016-02-27 02:07:33 -0600

    Moved duplicate code into include.

--- author.php
+++ author.php
@@ -16,29 +16,7 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/cover', 'author' ); ?>
 
-			<?php if ( have_posts() ) : ?>
-
-				<?php
-				$list_style = esc_attr( get_theme_mod( 'cover_list_style', 'minimal' ) );
-				$number_of_columns = esc_attr( get_theme_mod( 'cover_number_of_columns', 1 ) );
-				?>
-
-				<div id="posts" class="<?php echo $list_style; ?> <?php if ( 'grid' == $list_style && $number_of_columns > 1 ) { echo 'card-' . $number_of_columns; } ?>">
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php get_template_part( 'template-parts/content', 'summary' ); ?>
-
-					<?php endwhile; ?>
-				</div>
-
-				<?php the_posts_navigation(); ?>
-
-			<?php else : ?>
-
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-			<?php endif; ?>
+			<?php get_template_part( 'template-parts/content', 'loop' ); ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
