4ed56885e53819f0832e0c70657e76cfb83c139e
Paul <peiche@gmail.com>
2016-03-09 07:54:31 -0600

    Added array check and removed !important from css

--- inc/color-posts.php
+++ inc/color-posts.php
@@ -19,33 +19,35 @@ function cover_use_custom_colors( $colors_css, $color, $contrast ) {
     $post_id = get_the_ID();
 
     $tonesque = get_post_meta( $post_id, '_post_colors', true );
-    extract( $tonesque );
+    if ( is_array( $tonesque ) ) {
+      extract( $tonesque );
 
-    if ( 'ffffff' != $color ) {
-        $colors_css = '
-            .header,
-            .header a,
-            .header .site-description,
-            .cover {
-                color: rgb(' . $contrast . ') !important;
-            }
-            .header a:hover {
-                border-color: rgb(' . $contrast . ') !important;
-            }
-            .header .site-description {
-                border-color: rgba(' . $contrast . ', .25) !important;
-            }
-            .hamburger span,
-            .hamburger span:after,
-            .hamburger span:before {
-                background-color: rgb(' . $contrast . ') !important;
-            }
-            .header .backdrop {
-                background-color: #' . $color . ' !important;
-            }
-            ';
-    } else {
-        $colors_css = '';
+      if ( 'ffffff' != $color ) {
+          $colors_css = '
+              body .header,
+              body .header a,
+              body .header .site-description,
+              .cover {
+                  color: rgb(' . $contrast . ') !important;
+              }
+              .header a:hover {
+                  border-color: rgb(' . $contrast . ');
+              }
+              .header .site-description {
+                  border-color: rgba(' . $contrast . ', .25) !important;
+              }
+              .hamburger span,
+              .hamburger span:after,
+              .hamburger span:before {
+                  background-color: rgb(' . $contrast . ');
+              }
+              body .header .backdrop {
+                  background-color: #' . $color . ';
+              }
+              ';
+      } else {
+          $colors_css = '';
+      }
     }
 
     return $colors_css;
