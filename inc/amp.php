dbd11ef8efbd0497c453c819c04488e4e7b5c750
Paul <peiche@gmail.com>
2016-10-21 08:49:15 -0500

    Fixed wrong class name

--- inc/amp.php
+++ inc/amp.php
@@ -9,7 +9,7 @@ add_action( 'amp_post_template_css', 'cover_amp_post_template_css' );
 
 function cover_amp_post_template_css( $amp_template ) {
     ?>
-    nav.amp-wp-title-bar {
+    .amp-wp-header {
       background-color: <?php echo esc_attr( get_theme_mod( 'cover_header_color', '#026ed2' ) ); ?>
     }
     <?php
